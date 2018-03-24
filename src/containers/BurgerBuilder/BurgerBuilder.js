e7e7199ff78853339b17dfcdd021d9ee5db51b95
Viko <viktor.wrk@gmail.com>
2018-03-24 19:40:54 +1100

    BurgerBuilder component added

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+import Aux from '../../hoc/Util';
+
+
+class BurgerBuilder extends Component{
+
+    state={
+
+    };
+
+    render(){
+        return(
+            <Aux>
+                <div>Burger</div>
+                <div>Build Controls</div>
+            </Aux>
+        );
+    }
+}
+
+export default BurgerBuilder;
