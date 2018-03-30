47ddbcc5f48f74a27470bbebc22c1600968985df
Viko <viktor.wrk@gmail.com>
2018-03-30 12:43:08 +1100

    Top navigation component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,9 +2,11 @@ import React from 'react';
 import Aux from '../../hoc/Util';
 import Classes from './Layout.css';
 
+import Toolbar from '../Navigation/Toolbar/Toolbar';
+
 const layout = (props) => (
     <Aux>
-        <div>Toolbar, SideDrower, Backdrop</div>
+        <Toolbar />
         <main className={Classes.content}>
             {props.children}
         </main>
---
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems'
+/**
+*@returns JSX
+*/
+const toolbar = (props)=>(
+    <header className={classes.Toolbar}>
+        <div>Menu</div>
+        <Logo />
+        <NavigationItems />
+    </header>
+);
+
+export default toolbar;
