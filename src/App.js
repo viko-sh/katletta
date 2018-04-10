9de43f3d50a6d191808b86b30f180fa584222156
Viko <viktor.wrk@gmail.com>
2018-04-10 11:32:11 +1000

    Cleaning

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
-import {BrowserRouter, Route} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 
 
 class App extends Component {
--- src/index.js
+++ src/index.js
@@ -4,12 +4,42 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import {BrowserRouter} from 'react-router-dom';
+
+//redux
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {createStore, applyMiddleware, compose} from 'redux';
 import reducer from './store/reducer';
 
-const store = createStore(reducer);
 
+//middleware
+const logger = store => {
+    return next =>{
+        return action =>{
+            console.log('[Middleware]: Dispatching', action);
+            const result = next(action);
+
+            console.log('[Middleware] next state', store.getState());
+            return result;
+        }
+    }
+};
+
+const adminLogger = store => {
+    return next =>{
+        return action =>{
+            console.log('[ADMIN Middleware]: Dispatching', action);
+            const result = next(action);
+
+            console.log('[ADMIN Middleware] next state', store.getState());
+            return result;
+        }
+    }
+};
+
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(logger, adminLogger)));
 
 
 const app = (
